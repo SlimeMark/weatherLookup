bc9c87660d1dc52aa9241ba8279cbd6f0d116538
Hengjie Ma <hengjie_ma@outlook.com>
2024-09-24 20:51:54 +0800

    Fixed: Attempt to read JSON before write Added: Infinite loop for main program & force type to 'metar' as other functions currently N/A.

--- formatJSON.py
+++ formatJSON.py
@@ -1,32 +1,32 @@
 import json
 
 
-with open('response.json') as f:
-    data = json.load(f)
-
-
 def print_format_metar():
+    with open('response.json') as f:
+        data = json.load(f)
     for i in range(len(data['data'])):
+        print("\n")
         print(f"Station: {data['data'][i]['station']['name']}")
         print(f"Time: {data['data'][i]['observed']}")
         print(f"Temperature: {data['data'][i]['temperature']['celsius']}°C")
         print(f"Dewpoint: {data['data'][i]['dewpoint']['celsius']}°C")
         print(f"Wind: {data['data'][i]['wind']['degrees']}° at "
               f"{data['data'][i]['wind']['speed_kts']} knots")
-        print("Clouds: ")
-        print("+-----------------+")
-        cloud = {}
         try:
+            print("Clouds: ")
+            print("+-----------------+")
+            cloud = {}
             for j in range(len(data['data'][i]['clouds'])):
                 cloud[data['data'][i]['clouds'][j]['text']] \
                     = [int(data['data'][i]['clouds'][j]['base_feet_agl'])]
             for key, value in sorted(cloud.items(),
                                      key=lambda x: x[1], reverse=True):
                 print(f"{key} at {value[0]} feet AGL")
+            print("+-----------------+")
+            print(f"Ceiling: {data['data'][i]['ceiling']['feet']} feet")
         except KeyError:
-            pass
-        print("+-----------------+")
-        print(f"Ceiling: {data['data'][i]['ceiling']['feet']} feet")
+            print("+------CAVOK------+")
+            print("+-----------------+")
         print(f"Visibility: {data['data'][i]['visibility']['meters']} meters")
         print(f"Altimeter: {data['data'][i]['barometer']['hpa']} hPa")
         print(f"Flight Category: {data['data'][i]['flight_category']}")
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import savejson
 import formatJSON
 
-type = input("Enter the type of data you want to retrieve "
-             "(metar, taf, airmet/sigmet): ")
-station = input("Enter the station code (e.g. ZSSS): ")
-# latitude = input("Enter the latitude (e.g. 31.2): ")
-# longitude = input("Enter the longitude (e.g. 121.3): ")
-# radius = input("Enter the radius (e.g. 100): ")
-# options = input("Enter any options (e.g. decoded): ")
+type = 'metar'
 
-savejson.save_metar_taf(type, station, None, None, None, 'decoded')
-formatJSON.print_format_metar()
+while True:
+    """
+    type = input("Enter the type of data you want to retrieve "
+                 "(metar, taf, airmet/sigmet): ")
+    """
+    station = input("Enter the station code (e.g. ZSSS): ")
+    # latitude = input("Enter the latitude (e.g. 31.2): ")
+    # longitude = input("Enter the longitude (e.g. 121.3): ")
+    # radius = input("Enter the radius (e.g. 100): ")
+    # options = input("Enter any options (e.g. decoded): ")
+
+    savejson.save_metar_taf(type, station, None, None, None, 'decoded')
+    formatJSON.print_format_metar()
 
 # TODO: TAF and AIRMET/SIGMET inquires
