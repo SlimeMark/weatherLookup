155fded2eaac876aa21e5fba60178915e8bd7534
Hengjie Ma <hengjie_ma@outlook.com>
2024-09-25 13:51:34 +0800

    Fixed: Handling of cloud report without a ceiling altitude Added: Conditions decoding & SIG cloud (CB&TCU) alert

--- formatJSON.py
+++ formatJSON.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 
 def print_format_metar():
@@ -26,10 +27,25 @@ def print_format_metar():
                                      key=lambda x: x[1], reverse=True):
                 print(f"{key} at {value[0]} feet AGL")
             print("+-----------------+")
+            raw = data['data'][i]['raw_text']
+            if re.search('CB', raw):
+                print("Caution: CB reported")
+            if re.search('TCU', raw):
+                print("Caution: TCU reported")
             print(f"Ceiling: {data['data'][i]['ceiling']['feet']} feet")
         except KeyError:
-            print("+------CAVOK------+")
+            if not data['data'][i]['clouds']:
+                print("+------CAVOK------+")
+                print("+-----------------+")
+            print("No ceiling reported")
+        try:
+            print("Conditions: ")
             print("+-----------------+")
+            for k in range(len(data['data'][i]['conditions'])):
+                print(f"{data['data'][i]['conditions'][k]['text']}")
+            print("+-----------------+")
+        except KeyError:
+            print("No conditions reported")
         print(f"Visibility: {data['data'][i]['visibility']['meters']} meters")
         print(f"Altimeter: {data['data'][i]['barometer']['hpa']} hPa")
         print(f"Flight Category: {data['data'][i]['flight_category']}")
